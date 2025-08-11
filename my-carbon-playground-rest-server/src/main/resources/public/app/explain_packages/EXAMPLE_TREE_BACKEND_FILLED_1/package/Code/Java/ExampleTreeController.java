7c760663554faed3558d62f2d1a1cd4f7620263e
holgerringels <holgerringels@mac.fritz.box>
2025-08-11 18:59:32 +0200

    update docu

---
+++ my-carbon-playground-rest-server/src/main/resources/public/app/explain_packages/EXAMPLE_TREE_BACKEND_FILLED_1/package/Code/Java/ExampleTreeController.java
@@ -0,0 +1,195 @@
+package restserver.controller;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import common.constants.Constants_CT_ErrorCodes;
+import common.model.pojo.PojoErrorResult;
+import common.service.ServiceFactory;
+import common.service.ServiceLocator;
+import common.service.app.access.IAppDirectoryService;
+import common.utils.jgrapht.helper.JGraphTListToTreeTransformer;
+import common.utils.jgrapht.pojo.PojoJGraphtTreeNode;
+import restserver.objects.tree.generator.DataGeneratorForDirectory;
+import restserver.objects.tree.generator.SimpleDummyDataGenerator;
+import restserver.objects.tree.generator.SimpleDummyDataGeneratorViaList;
+
+@RestController
+public class ExampleTreeController {
+
+	// *************************
+	// * Approach 1 
+	// ************************************************************************************************************************
+	public static final String EXAMPLE_RECURSIVE_TREE_APPROACH_1 = "/Example/TreeViaRecursion";
+	// ************************************************************************************************************************
+	/**
+	 * This method retrieves a tree of all entities related to a Profile<br/>
+	 * @param profileID Profile's ID  
+	 * @return Tree's Root-Elements
+	 */
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET, value = EXAMPLE_RECURSIVE_TREE_APPROACH_1)
+	public ResponseEntity<Object> doGetModelEntitiesDirectRecursive() {
+
+		String endPointInfo = EXAMPLE_RECURSIVE_TREE_APPROACH_1 + " GET"; 
+
+		Logger logger = LoggerFactory.getLogger(ExampleTreeController.class);
+		logger.info("");
+		logger.info(endPointInfo);
+		logger.info("");
+
+		ResponseEntity<Object> result = null;
+
+		try {
+			//========== >
+
+			//don't worry to much about this ... it is just a way to gain access to explain_packages
+			//in your concrete situation you can choose any java.nio.file.Path as pRoot
+			ServiceLocator sl = ServiceFactory.getInstance().getServiceLocator();
+			IAppDirectoryService appDirectoryService = sl.getAppDirectoryService();
+			Path pRoot = appDirectoryService.getAppDirectory("explain_packages");
+
+			
+			//get data as Graph without any edges
+			SimpleDirectedGraph<PojoJGraphtTreeNode, DefaultEdge> graph = DataGeneratorForDirectory.gatherEntitiesTree(pRoot);
+
+			//identify root-nodes
+			PojoJGraphtTreeNode[] array = JGraphTListToTreeTransformer.getTreeRootNodes(graph);
+			
+			
+			//<==========
+
+			result =  new ResponseEntity<>(array,HttpStatus.OK);
+			return result;
+
+		} catch (Exception e) {
+			logger.error("", e);
+			PojoErrorResult pojoErrorResult = new PojoErrorResult("", Constants_CT_ErrorCodes.NO_ERROR_CODE, e);
+			ResponseEntity<Object> responseEntity 
+			= new ResponseEntity<Object>(pojoErrorResult, HttpStatus.INTERNAL_SERVER_ERROR);
+			return responseEntity;		
+		}
+	
+	}
+	
+	
+	
+
+	// *************************
+	// * Approach 2 
+	// ************************************************************************************************************************
+	public static final String EXAMPLE_TREE_APPROACH_2 = "/Example/TreeViaList";
+	// ************************************************************************************************************************
+	/**
+	 * This method retrieves a tree of all entities related to a Profile<br/>
+	 * @param profileID Profile's ID  
+	 * @return Tree's Root-Elements
+	 */
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET, value = EXAMPLE_TREE_APPROACH_2)
+	public ResponseEntity<Object> doGetModelEntitiesViaList() {
+
+		String endPointInfo = EXAMPLE_TREE_APPROACH_2 + " GET"; 
+
+		Logger logger = LoggerFactory.getLogger(ExampleTreeController.class);
+		logger.info("");
+		logger.info(endPointInfo);
+		logger.info("");
+
+		ResponseEntity<Object> result = null;
+
+		try {
+			
+			//========== >
+
+			//get data as Graph without any edges
+			SimpleDirectedGraph<PojoJGraphtTreeNode, DefaultEdge> graph = SimpleDummyDataGeneratorViaList.gatherEntitiesTree();
+
+			//identify root-nodes
+			PojoJGraphtTreeNode[] array = JGraphTListToTreeTransformer.getTreeRootNodes(graph);
+			
+			//sort array by label
+			Arrays.sort(array, Comparator.comparing(node -> node.getLabel()));
+			
+			//<==========
+
+			result =  new ResponseEntity<>(array,HttpStatus.OK);
+			return result;
+
+		} catch (Exception e) {
+			logger.error("", e);
+			PojoErrorResult pojoErrorResult = new PojoErrorResult("", Constants_CT_ErrorCodes.NO_ERROR_CODE, e);
+			ResponseEntity<Object> responseEntity 
+			= new ResponseEntity<Object>(pojoErrorResult, HttpStatus.INTERNAL_SERVER_ERROR);
+			return responseEntity;		
+		}
+	
+	}
+	
+	
+	
+	// *************************
+	// * Tree Direct Example
+	// ************************************************************************************************************************
+	public static final String EXAMPLE_TREE_APPROACH_1 = "/Example/Tree/Direct/Example";
+	// ************************************************************************************************************************
+	/**
+	 * This method retrieves a tree of all entities related to a Profile<br/>
+	 * @param profileID Profile's ID  
+	 * @return Tree's Root-Elements
+	 */
+	@CrossOrigin
+	@RequestMapping(method = RequestMethod.GET, value = EXAMPLE_TREE_APPROACH_1)
+	public ResponseEntity<Object> doGetModelEntitiesDirect() {
+
+		String endPointInfo = EXAMPLE_TREE_APPROACH_1 + " GET"; 
+
+		Logger logger = LoggerFactory.getLogger(ExampleTreeController.class);
+		logger.info("");
+		logger.info(endPointInfo);
+		logger.info("");
+
+		ResponseEntity<Object> result = null;
+
+		try {
+			//========== >
+
+			//get data as Graph without any edges
+			SimpleDirectedGraph<PojoJGraphtTreeNode, DefaultEdge> graph = SimpleDummyDataGenerator.gatherEntitiesTree();
+
+			//identify root-nodes
+			PojoJGraphtTreeNode[] array = JGraphTListToTreeTransformer.getTreeRootNodes(graph);
+			
+			//sort array by label
+			Arrays.sort(array, Comparator.comparing(node -> node.getLabel()));
+			
+			//<==========
+
+			result =  new ResponseEntity<>(array,HttpStatus.OK);
+			return result;		
+
+		} catch (Exception e) {
+			logger.error("", e);
+			PojoErrorResult pojoErrorResult = new PojoErrorResult("", Constants_CT_ErrorCodes.NO_ERROR_CODE, e);
+			ResponseEntity<Object> responseEntity 
+			= new ResponseEntity<Object>(pojoErrorResult, HttpStatus.INTERNAL_SERVER_ERROR);
+			return responseEntity;		
+		}
+	
+	}
+	
+	
+	
+}
---
+++ my-carbon-playground-rest-server/src/main/resources/public/app/explain_packages/EXAMPLE_TREE_BACKEND_FILLED_1/package/Code/JavaScript/Tree_03_BackendFilledViaList/Tree.js
@@ -0,0 +1,80 @@
+'use client';
+
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { TreeView, TreeNode, InlineLoading } from '@carbon/react';
+
+import * as RESTClient from './backend/RestClient';
+import * as NodeContentProvider from './uihelper/NodeContentProvider';
+import * as NodeIconProvider from './uihelper/NodeIconProvider';
+
+class Tree extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize state
+    this.state = {
+      id: this.constructor.name + ' ' + uuidv4(),
+      treeData: null,
+    };
+  }
+
+  /**
+   * The componentDidMount() -method retrieves the treeData from the
+   * REST-Server and stores it to this component's state
+   */
+  async componentDidMount() {
+    let dataFromRestService = await RESTClient.captureTree(null);
+    this.setState({
+      treeData: dataFromRestService
+    });
+  }
+
+
+  /**
+    * This method rendes the nodes recursively to User Interface
+    * @param {*} nodes List of Nodes
+    * @param {*} hrefRenderTreeNodes Pass-Through of render-method
+    * @returns a rendered Tree Node with all it's children
+    */
+  renderTreeNodes = (nodes, hrefRenderTreeNodes) =>
+    nodes.map((node) => (
+      <TreeNode
+        key={node.id}
+        label={NodeContentProvider.getNodeContent(node)}
+        isExpanded={false}
+        renderIcon={NodeIconProvider.getIcon(node)}
+      >
+        {node.children && node.children.length > 0
+          ? hrefRenderTreeNodes(node.children, hrefRenderTreeNodes)
+          : null}
+      </TreeNode>
+    ));
+
+
+  render() {
+
+    let hrefRenderTreeNodes = this.renderTreeNodes.bind(this)
+
+    const { treeData } = this.state;
+
+    return (
+      <div>
+        {treeData === null
+          ? 
+          <InlineLoading
+            description="..."
+            iconDescription="..." />
+          :
+          <TreeView
+            label="IBM Carbon TreeView"
+          >
+            {hrefRenderTreeNodes(treeData, hrefRenderTreeNodes)}
+          </TreeView>
+        }
+      </div>
+    );
+  }
+}
+
+export default Tree;
